36cddb6b187a84d8babec22c7cf930cb5937567a
Michael Piepgras Neergaard <michael@neergaard.dk>
2023-01-20 10:07:13 +0100

    Added react routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './views/Login';
+import Profile from './views/Profile';
+import Translator from './views/Translator';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={ <Login/> } />
+          <Route path="/translator" element={ <Translator/> } />
+          <Route path="/profile" element={ <Profile/> } />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/views/Translator.jsx
@@ -0,0 +1,6 @@
+const Translator = () => {
+    return (
+        <h1>Translator</h1>
+    )
+}
+export default Translator
