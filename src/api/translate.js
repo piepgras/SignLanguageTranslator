e37335c946359a6e1159268b71ee7e90f93dde03
Michael Piepgras Neergaard <michael@neergaard.dk>
2023-01-24 17:45:16 +0100

    Deleted context folder

--- src/api/translate.js
+++ src/api/translate.js
@@ -1,22 +1,33 @@
 import { createHeader } from "./index";
+import { STORAGE_KEY_USER } from "../const/storageKeys";
+import { useUser } from "../state/UserState";
 
 const API_URL = process.env.REACT_APP_API_URL
 
 export const addTranslation = async (body) => {
-    await fetch(`${API_URL}`, {
-        method: 'POST',
-        headers: createHeader(),
-        body: JSON.stringify(body)
-    })
+    try {
+        console.log(`${API_URL}?username=${useUser}`)
+        await fetch(`${API_URL}?username=${STORAGE_KEY_USER}`, {
+            method: 'POST',
+            headers: createHeader(),
+            body: JSON.stringify({translations: [body]})
+        })
+    } catch (error) {
+        return [error.message, null]
+    }
 }
 
 export const deleteTranslation = async (translation) => {
-    translation.status = "inactive";
-    const response = await fetch(`${API_URL}` + translation.id, {
-        method: 'PATCH',
-        headers: createHeader(),
-        body: JSON.stringify(translation)
-    })
-    const data = await response.json()
-    return data;
+    try {
+        translation.status = "inactive";
+        const response = await fetch(`${API_URL}` + translation.id, {
+            method: 'PATCH',
+            headers: createHeader(),
+            body: JSON.stringify(translation)
+        })
+        const data = await response.json()
+        return data;
+    } catch (error) {
+        return [error.message, null]
+    }
 }
