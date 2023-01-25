34b2dda79d190237ca323b7594a74adfac639a21
Michael Piepgras Neergaard <michael@neergaard.dk>
2023-01-25 18:43:19 +0100

    Refactored

--- src/api/translate.js
+++ src/api/translate.js
@@ -1,19 +1,29 @@
+import { STORAGE_KEY_USER } from "../const/storageKeys";
+import { useUser } from "../state/UserState";
+import { storageRead } from "../utils/storage";
 import { createHeader } from "./index";
 
 const API_URL = process.env.REACT_APP_API_URL
 
+
 export const addTranslation = async (user, translation) => {
+    console.log(user.username + " " + translation)
     try {
-        console.log(`${API_URL}?username=${user.username}`)
-        await fetch(`${API_URL}?username=${user.username}`, {
+        const response = await fetch(`${API_URL}/${user.id}`, {
             method: 'PATCH',
             headers: createHeader(),
             body: JSON.stringify({
-                username: user.username,
-                translations: [...user.translations, translation]
-                
+                translations: [...user.translations, translation] 
             })
         })
+
+        if(!response.ok){
+            throw new Error("Could not update the translation")
+        }
+
+        const result = await response.json()
+        return [ null, result ]
+
     } catch (error) {
         return [error.message, null]
     }
--- src/compontents/translate/TranslateForm.jsx
+++ src/compontents/translate/TranslateForm.jsx
@@ -7,15 +7,21 @@ import { storageRead } from "../../utils/storage";
 const TranslateForm = () => {
     const [inputText, setInputText] = useState(" ")
     const [images, setImages] = useState([]);
+    let user = storageRead(STORAGE_KEY_USER)
 
 
 const handleChange = (event) => {
     setInputText(event.target.value);
 }
 
+function parseTranslation(str) {
+    return str.toLowerCase().replace(/[^a-z]/g, "");
+}
+
 const handleButtonClick = () => {
-    addTranslation(storageRead(STORAGE_KEY_USER), inputText)
-    setImages(inputText.split('').map((char, index) => (
+    let parsedTranslation = parseTranslation(inputText)
+    addTranslation(user, parsedTranslation)
+    setImages(parsedTranslation.split('').map((char, index) => (
         <img className="sign" key={index} src={`../signs/${char}.png`} alt={char} />
     )));
 }
