b5447f4ca41374927d9464016b66a858f2915af1
“Farhang <farhang_younis@hotmail.com>
2023-01-26 11:26:35 +0100

    Renamed profileHeader and login and added som styling

--- src/compontents/Profile/ProfileHeader.jsx
+++ src/compontents/Profile/ProfileHeader.jsx
@@ -2,7 +2,7 @@ const ProfileHeader = ({username}) => {
     return (
 
         <header>
-            <h5>Welcome home! {username}</h5>
+            <h5 className="text-light">Welcome home! {username}</h5>
         </header>
         
     )
